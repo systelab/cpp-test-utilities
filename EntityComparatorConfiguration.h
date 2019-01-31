ef9962e61dc0e6bf40d9559989d6e3fd217bae87
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-01-31 15:19:48 +0100

    Added entity comparison utilities

---
+++ EntityComparatorConfiguration.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace systelab { namespace test_utility {
+
+	class EntityComparatorConfiguration
+	{
+	public:
+		inline EntityComparatorConfiguration();
+		inline EntityComparatorConfiguration(std::vector<std::string>);
+		inline virtual ~EntityComparatorConfiguration();
+
+		inline bool isMemberExcluded(const std::string&) const;
+
+	private:
+		std::vector<std::string> m_excludedMembers;
+	};
+
+}}
+
+#include "EntityComparatorConfiguration.inl"
