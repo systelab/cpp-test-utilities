55654ee17467b319f33de9a42a0c5e0fa0aebb9b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-11 20:51:54 +0100

    Added conan recipie and basic integration for Travis & AppVeyor

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+
+class TestUtilitiesInterfaceConan(ConanFile):
+    name = "TestUtilitiesInterface"
+    description = "C++ Test Utilities interface"
+    url = "https://github.com/systelab/cpp-test-utilities"
+    homepage = "https://github.com/systelab/cpp-test-utilities"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "test", "utilities", "gtest")	
+    license = "MIT"
+    generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def package(self):
+        self.copy("*.h", dst="include/TestUtilitiesInterface", src="TestUtilitiesInterface")
+
+    def package_info(self):
+        self.info.header_only()
